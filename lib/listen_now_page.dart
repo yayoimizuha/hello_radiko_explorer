48b19dcdfd4b1889c6608e9732c0ac53f9f97a07
yayoimizuha <poko.peko.tomo@icloud.com>
2025-02-17 18:40:10 +0900

    aaa

--- lib/listen_now_page.dart
+++ lib/listen_now_page.dart
@@ -10,9 +10,12 @@ class ListenNowPage extends StatefulWidget {
   State<ListenNowPage> createState() => _ListenNowPageState();
 }
 
+void getFirebaseStruct(String name) {}
+
 class _ListenNowPageState extends State<ListenNowPage> {
   List<String> _selectedMembers = [];
   List<String> _selectedGroups = [];
+  final List<String> _allSelectedItems = [];
 
   @override
   void initState() {
@@ -46,6 +49,8 @@ class _ListenNowPageState extends State<ListenNowPage> {
       setState(() {
         _selectedMembers = extractSelectedKeys(memberSelections);
         _selectedGroups = extractSelectedKeys(groupSelections);
+        _allSelectedItems.addAll(_selectedGroups);
+        _allSelectedItems.addAll(_selectedMembers);
       });
     } catch (e) {
       print('Error loading selections: $e');
@@ -66,14 +71,14 @@ class _ListenNowPageState extends State<ListenNowPage> {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text('選択されたメンバー:'),
+              const Text('選択されたアイテム:'),
 
               _selectedMembers.isEmpty
                   ? const Text('選択されたメンバーはいません')
                   : Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children:
-                        _selectedMembers
+                        _allSelectedItems
                             .map(
                               (member) => Padding(
                                 padding: const EdgeInsets.only(
@@ -82,27 +87,6 @@ class _ListenNowPageState extends State<ListenNowPage> {
                                   bottom: 4.0,
                                 ),
                                 child: Text('・$member'),
-                              ),
-                            )
-                            .toList(),
-                  ),
-
-              const SizedBox(height: 16),
-              const Text('選択されたグループ:'),
-              _selectedGroups.isEmpty
-                  ? const Text('選択されたグループはいません')
-                  : Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children:
-                        _selectedGroups
-                            .map(
-                              (group) => Padding(
-                                padding: const EdgeInsets.only(
-                                  left: 16.0,
-                                  top: 4.0,
-                                  bottom: 4.0,
-                                ),
-                                child: Text('・$group'),
                               ),
                             )
                             .toList(),
