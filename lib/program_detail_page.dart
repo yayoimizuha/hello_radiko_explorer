4ce067637bfb4bbd16b9d166c16a0b0b1f25c731
yayoimizuha <poko.peko.tomo@icloud.com>
2025-02-20 21:08:54 +0900

    aaa

--- lib/program_detail_page.dart
+++ lib/program_detail_page.dart
@@ -21,7 +21,7 @@ class ProgramDetailPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Flexible(
-              child: Text(program.title, overflow: TextOverflow.ellipsis,),
+              child: Text(program.title, overflow: TextOverflow.ellipsis),
             ),
             IconButton(
               icon: const Icon(Icons.open_in_new),
@@ -31,17 +31,17 @@ class ProgramDetailPage extends StatelessWidget {
                 final webUrl =
                     'https://radiko.jp/#!/ts/${program.radioChannel.id}/${DateFormat('yyyyMMddHHmmss').format(program.ft)}';
 
-                if (await canLaunchUrl(Uri.parse(radikoUrl))) {
-                  await launchUrl(Uri.parse(radikoUrl));
+                // if (await canLaunchUrl(Uri.parse(radikoUrl))) {
+                await launchUrl(Uri.parse(radikoUrl));
+                // } else {
+                if (await canLaunchUrl(Uri.parse(webUrl))) {
+                  await launchUrl(Uri.parse(webUrl));
                 } else {
-                  if (await canLaunchUrl(Uri.parse(webUrl))) {
-                    await launchUrl(Uri.parse(webUrl));
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Radikoアプリもブラウザも開けませんでした。')),
-                    );
-                  }
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Radikoアプリもブラウザも開けませんでした。')),
+                  );
                 }
+                // }
               },
             ),
           ],
